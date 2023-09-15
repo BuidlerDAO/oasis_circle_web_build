04c1f0e6ac7be9337672ec77fd5de0fc99d4d42e
aboutmydreams <aboutmydreams@163.com>
2023-09-15 09:40:13 +0000

    Testing push with GitHub Actions

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,11 +26,11 @@ const RESOURCES = {"canvaskit/chromium/canvaskit.js": "8c8392ce4a4364cbb240aa09b
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
-"index.html": "6ce360fa4c86a9a1d86c7d2d4c86fe61",
-"/": "6ce360fa4c86a9a1d86c7d2d4c86fe61",
+"index.html": "6e2643f0e63502ec962ab7e89bf57724",
+"/": "6e2643f0e63502ec962ab7e89bf57724",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 ".github/workflows/main.yml": "ce233e92b5875918d859e9c9d9b9ffc7",
-"main.dart.js": "8cd9ef90a3f9b58489f0ebe79c9df013",
+"main.dart.js": "63af2709b471eba211e36de1504c334c",
 "vercel.json": "b86674833d99981d58ca921aeb97b92a",
 "manifest.json": "b821ad466feece003cce6da9f796e5f5"};
 // The application shell files that are downloaded before a service worker can
